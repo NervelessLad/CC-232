c4187a56979b8997e393a5923a3a5bb4c82be692
kapumota <121532729+kapumota@users.noreply.github.com>
2026-03-21 05:14:45 -0500

    demo_hanoi.cpp

--- Libreria_cc232/Semana1/demos/demo_hanoi.cpp
+++ Libreria_cc232/Semana1/demos/demo_hanoi.cpp
@@ -7,7 +7,7 @@ int main() {
     std::vector<HanoiMove> moves;
     solveHanoi(3, 'A', 'B', 'C', moves);
 
-    std::cout << "Movimientos para Hanoi con 3 discos:\n";
+    std::cout << "Movimientos para Hanoi con 3 discos (ejemplo):\n";
     for (const auto& [from, to] : moves) {
         std::cout << from << " -> " << to << "\n";
     }
