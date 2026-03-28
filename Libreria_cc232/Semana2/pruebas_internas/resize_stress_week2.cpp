9813710e5586b42f067f7582b3315d23d1b04dfc
kapumota <121532729+kapumota@users.noreply.github.com>
2026-03-28 09:51:46 -0500

    Pruebas internas CC232

--- Libreria_cc232/Semana2/pruebas_internas/resize_stress_week2.cpp
+++ Libreria_cc232/Semana2/pruebas_internas/resize_stress_week2.cpp
@@ -2,6 +2,7 @@
 #include "ArrayStack.h"
 #include "FastArrayStack.h"
 #include "RootishArrayStack.h"
+#include "DengVector.h"
 
 int main() {
     {
@@ -23,6 +24,19 @@ int main() {
         for (int i = 0; i < 120; ++i) s.add(i, i);
         for (int i = 119; i >= 60; --i) assert(s.remove(i) == i);
         assert(s.size() == 60);
+    }
+
+    {
+        ods::DengVector<int> v;
+        for (int i = 0; i < 1000; ++i) v.insert(v.size(), i);
+        assert(v.size() == 1000);
+        for (int i = 0; i < 700; ++i) assert(v.remove(0) == i);
+        assert(v.size() == 300);
+        for (int i = 700; i < 1000; ++i) assert(v[i - 700] == i);
+        for (int i = 1000; i < 1300; ++i) v.insert(v.size(), i);
+        assert(v.size() == 600);
+        for (int i = 0; i < 300; ++i) assert(v[i] == i + 700);
+        for (int i = 300; i < 600; ++i) assert(v[i] == i + 700);
     }
 
     return 0;
--- Libreria_cc232/Semana2/pruebas_internas/test_internal_capitulo2_extendido.cpp
+++ Libreria_cc232/Semana2/pruebas_internas/test_internal_capitulo2_extendido.cpp
@@ -2,6 +2,14 @@
 #include <cmath>
 #include "Capitulo2.h"
 
+namespace {
+struct IncreaseByTen {
+    void operator()(int& x) const {
+        x += 10;
+    }
+};
+}
+
 int main() {
     ods::array<int> a(4);
     for (int i = 0; i < a.length; ++i) a[i] = i * 10;
@@ -35,6 +43,46 @@ int main() {
     for (int i = 0; i < 50; ++i) assert(rootish.get(i) == i * 2);
     assert(rootish.remove(10) == 20);
     assert(rootish.get(10) == 22);
+
+    ods::DengVector<int> v;
+    for (int i = 0; i < 64; ++i) {
+        v.insert(v.size(), i);
+    }
+    assert(v.size() == 64);
+    assert(v.capacity() >= 64);
+    for (int i = 0; i < 64; ++i) {
+        assert(v[i] == i);
+    }
+
+    assert(v.remove(10, 20) == 10);
+    assert(v.size() == 54);
+    assert(v[10] == 20);
+
+    ods::DengVector<int> copy(v);
+    copy[0] = -100;
+    assert(v[0] == 0);
+    assert(copy[0] == -100);
+
+    ods::DengVector<int> assigned;
+    assigned = v;
+    assert(assigned.size() == v.size());
+    for (int i = 0; i < assigned.size(); ++i) {
+        assert(assigned[i] == v[i]);
+    }
+
+    IncreaseByTen inc;
+    assigned.traverse(inc);
+    assert(assigned[0] == 10);
+    assert(assigned[assigned.size() - 1] == v[v.size() - 1] + 10);
+
+    for (int i = 0; i < 44; ++i) {
+        assigned.remove(0);
+    }
+    assert(assigned.size() == 10);
+    assert(assigned.capacity() >= assigned.size());
+    assert(assigned.find(73) == 9);
+    assert(assigned.find(63) == -1);
+    assert(assigned.find(999) == -1);
 
     ods::FastSqrt::init();
     for (int x = 0; x <= 50000; ++x) {
